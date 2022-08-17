744e16a30857ed77a42f3c304ce5295f615e7c47
Arsalan Rehman <arsalanrahman008@gmail.com>
2022-08-17 19:55:05 +0300

    first commit

---
+++ src/components/Users/Adduser.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useState } from 'react'
+import Button from '../UI/Button'
+import Card from '../UI/Card'
+import ErrorModal from '../UI/ErrorModal'
+import classes from './AddUser.module.css'
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    
+    event.preventDefault();
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        setError({
+          title: 'Username Error',
+          message : 'Please use a valid username'
+        }
+        );
+        return;
+    }
+    if (+enteredAge < 1){
+      setError({
+        title: 'Age Error',
+        message : 'Please imput valid age (>0)'
+      }
+      );
+      return;
+    }
+
+    
+    props.onAddUser( enteredUsername, enteredAge );
+    setEnteredAge('');
+    setEnteredUsername('');
+    
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value)
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value)
+  };
+  const errorHandler =() => {
+    setError(null);
+  };
+
+  return (
+    <div>
+   {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          value={enteredUsername}
+          type="text"
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button onConfirm={errorHandler} type="submit"> Add User</Button>
+      </form>
+    </Card>
+    </div>
+  );
+}
+export default AddUser;
